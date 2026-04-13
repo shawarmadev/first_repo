6e24c811913e89e23330ca01430838c045f1047c
TomDumont <tom.dumont1993@gmail.com>
2026-04-13 17:52:24 +0200

    scorePassing

--- Coding2026/Test.java
+++ Coding2026/Test.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
 public class Test {
 
+  @SuppressWarnings("ConvertToTryWithResources")
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    
+    Scanner scan = new Scanner(System.in);
+
+
+      System.out.println("Enter your score :");
+      int score = scan.nextInt();
+
+      
+      if (score >= 50) 
+        System.out.println("You passed!");
+      else
+        System.out.println("You didn't pass.");
+      scan.close();
+    
   }
 }
